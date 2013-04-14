a190657677864d417a50344963cbe9014c4ae746
José Moreira <zemanel@zemanel.eu>
2013-04-14 07:29:39 +0100

    add rough initial version

---
+++ content/articles/blog-post-1.md
@@ -0,0 +1,28 @@
+Title: My super title
+Date: 2010-12-03 10:20
+Tags: thats, awesome
+Category: yeah
+Slug: my-super-post
+Summary: Short version for index and feeds
+
+This is the content of my super blog post.
+
+A block of Python.
+
+    :::python
+    AUTHOR = u'Jos\xe9 Moreira'
+    SITENAME = u'zemanel.eu'
+    SITEURL = 'http://www.zemanel.eu'
+    FEED_DOMAIN = SITEURL
+
+    def myfunc():
+        pass
+
+A block of Javascript.
+
+    :::javascript
+    function test() {
+        document.ready(function(){
+
+        });
+    }
---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*- #
+import os
+
+SITEDIR = os.path.realpath(os.path.dirname(__file__))
+
+AUTHOR = u'Jos\xe9 Moreira'
+SITENAME = u'zemanel.eu'
+SITEURL = 'http://www.zemanel.eu'
+FEED_DOMAIN = SITEURL
+TIMEZONE = 'Europe/Lisbon'
+GOOGLE_ANALYTICS = ''
+DEFAULT_LANG = u'en'
+
+## Appearence
+THEME = os.path.join(SITEDIR, 'theme/zen')
+
+## content
+#DELETE_OUTPUT_DIRECTORY = False
+OUTPUT_PATH = '_output'
+ARTICLE_DIR = 'articles'
+PAGE_DIR = 'pages'
+FILES_TO_COPY = [
+    (os.path.join(SITEDIR, 'vendor/bootstrap/img/glyphicons-halflings.png'), 'img/glyphicons-halflings.png'),
+    (os.path.join(SITEDIR, 'vendor/bootstrap/img/glyphicons-halflings-white.png'), 'img/glyphicons-halflings-white.png'),
+]
+
+# Social
+TWITTER_USERNAME = 'zemanel'
+SOCIAL = (
+    ('Twitter', 'http://twitter.com/zemanel'),
+    ('Github', 'http://github.com/zemanel'),
+    ('Linkedin', 'http://pt.linkedin.com/in/josemoreira'),
+    ('Stackoverflow', 'http://stackoverflow.com/users/2278823/zemanel'),
+    ('Careers 2.0', 'http://careers.stackoverflow.com/zemanel'),
+
+)
+
+DEFAULT_PAGINATION = 10
