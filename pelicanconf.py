4b7ad9e4c1a4d2396b288036ad86912f3c3de4e8
José Moreira <zemanel@zemanel.eu>
2013-04-16 19:05:56 +0100

    enable sitemap plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,6 +17,27 @@ TIMEZONE = 'Europe/Lisbon'
 GOOGLE_ANALYTICS = ''
 DEFAULT_LANG = u'en'
 
+# Plugins
+PLUGIN_PATH = SITEDIR.join('vendor/pelican_plugins')
+PLUGINS = [
+    'pelican.plugins.sitemap',
+]
+
+# Sitemap plugin
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
 ## Appearence
 THEME = os.path.join(SITEDIR, 'theme/zen')
 
